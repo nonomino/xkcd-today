9f149067932cacaee3408e239dfa0e56eba6d921
EugeneFelix <ontharros@gmail.com>
2022-02-21 12:36:54 +0530

    commit

--- api/xkcd.py
+++ api/xkcd.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def say_hello():
-    print("Hello, there\n")
-say_hello()
+    return "Hello, there\n"
 
 if __name__ == "__main__":
     app.run(debug=True)
