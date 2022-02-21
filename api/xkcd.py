d553636f577aa0176c7d285aeeb1dd7d72cbd537
EugeneFelix <ontharros@gmail.com>
2022-02-21 12:54:17 +0530

    commit

--- api/xkcd.py
+++ api/xkcd.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def say_hello():
-    return "Hello, there\n"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def hello():
+    return 'Hello, world'
