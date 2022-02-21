554321e0fdab050ee539ef43e162591c0be6f9dd
EugeneFelix <ontharros@gmail.com>
2022-02-21 21:20:46 +0530

    commit

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from urllib.request import urlopen
-import json
+import jsonom
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 app = Flask(__name__)
 BASE_URL = "https://xkcd-api-ridvanaltun.vercel.app/api/comics/random"
@@ -11,3 +13,14 @@ def read_base():
     data = json.loads(response.read())
     xkcd_url = data["img"]
     return render_template('img.html', img_url = xkcd_url)
+@app.route('/comic')
+def image_url():
+    url = './templates/img.html'
+    with HTMLSession() as session:
+    response = session.get(url)
+    response.html.render()
+    content = response.html.html
+    soup = BeautifulSoup(content, "html.parser")
+    images = soup.find_all("img")
+    src = images.get("src")
+    return src
