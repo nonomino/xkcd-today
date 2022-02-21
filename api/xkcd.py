458f5cd927f63d03eac9f12d31a6dd5798c7e3fc
EugeneFelix <ontharros@gmail.com>
2022-02-21 12:59:19 +0530

    commit

--- api/xkcd.py
+++ api/xkcd.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, world'
+
+@app.route('/test')
+def test():
+    return 'Test'
+
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('index.html', result = dict)
